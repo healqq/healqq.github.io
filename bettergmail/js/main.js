b6a71e5527758341bbe08ce5f0698ae85840f308
healqq <nyashin92@gmail.com>
2014-12-06 00:31:21 +0300

    angular 4

--- bettergmail/js/main.js
+++ bettergmail/js/main.js
@@ -4,15 +4,34 @@ app.controller('mainCtrl',['gmailService', '$scope', '$http', '$window',
 	
 		
 		$scope.token = gmailService.getToken();
+		$scope.loadEmails = (function(){
+			gmailService.sendRequest('list');
+		});
 		
 		
 	}	
 ]);
 
 app.provider('gmailService', [ function (){
-		
-		var sendRequest =  ( function( type){
+		/*request types param filler*/
+		var getRequestParams =  ( function( type, queryParams ){
+			var params = {};
+			if (queryParams == undefined){
+				 queryParams = {};
+			}
+			switch ( type ) {
+				'list': params = 
+					{ 
+						method 	: 'get',
+						url		: 'https://www.googleapis.com/gmail/v1/users/me/messages',
+						params  : queryParams,
+						
+					}
+				break;
+			}
 		});
+		
+		
 		
 		
 		return {
@@ -22,11 +41,26 @@ app.provider('gmailService', [ function (){
 				gWrap.parseResponse();
 				_token =  gWrap.checkState();
 				
+				var _sendRequest = ( function ( type, params ){
+					var requestParams = getRequestParams ( type, params );
+					
+					//adding access_token param to params
+					requestParams.params['access_token'] = _token;
+					//sending request
+					$http( requestParams).success( 
+						function(data){
+							console.log(data);
+						}
+					);
+				});
+				
 				return{
 					getToken: function(){
 						return _token;
 					},
-					sendRequest: function(){
+					sendRequest: function(type, params){
+						_sendRequest( type, params);
+						
 					}
 					
 				}
