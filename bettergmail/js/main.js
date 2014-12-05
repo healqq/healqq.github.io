153a75a4c0d147a6f6c1bc4d3258f0dc24934a3a
healqq <nyashin92@gmail.com>
2014-12-05 23:52:05 +0300

    angular

---
+++ bettergmail/js/main.js
@@ -0,0 +1,35 @@
+var app = angular.module('mailApp',[]);
+app.controller('mainController',['gmailService', '$scope', '$http', '$window',
+	function(gmailService, $scope, $http, $window ){
+	
+		token = gmailService.getToken();
+		
+		
+]);
+
+app.provider('gmailService', [ function (){
+		
+		var sendRequest =  ( function( type){
+		});
+		
+		
+		return {
+			$get: function($http){
+				var _token  = null;
+				var gWrap = gmailAuthWrapper();
+				if ( !gWrap.parseResponse() ){
+					token =  gWrap.checkState();
+				}
+				return{
+					getToken: function(){
+						return _token;
+					},
+					sendRequest: function(){
+					}
+					
+				}
+			}
+		}
+	
+	}
+]);	
