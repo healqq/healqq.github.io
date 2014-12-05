ca7261a382404d533c3542d83d72629e3509efd4
healqq <nyashin92@gmail.com>
2014-12-05 22:57:21 +0300

    gmail wrapper

--- bettergmail/js/gmail_wrap.js
+++ bettergmail/js/gmail_wrap.js
@@ -1,52 +1,71 @@
-var vkManager = (function(){
-	var ready = false;
-	var init =( function(){
-		if ( VK === undefined){
-			 return false;
-		}
-		VK.init({
-			apiId: 4479322
-		});
-		return true;
-	});	
-				//checking status
-	this.loginStatus = ( function( func ){
-		if (ready)
-			VK.Auth.getLoginStatus( func );
-		}
-	);
-				//login
-	this.login = (function ( func ){
-		if (ready)
-			VK.Auth.login(func);
-		}
-	);
-				//logout
-	this.logout = (function (func ){
-		if (ready)
-			VK.Auth.logout(func);
-		}
-	);
-				//getting user name
-	this.getUserInfo = ( function (uid, func){
-		if (ready)
-			VK.Api.call('users.get', {uids: uid}, func);
-		}
-	);
+var gmailWrapper = ( function(){
 	
-	this.getPoll = ( function ( pollId, func){
-		if (ready)
-			VK.Api.call('polls.getById', {poll_id: pollId}, func);
+	//google app params
+	var scope = 'email%20profile%20https://www.googleapis.com/auth/gmail.modify';
+	var clientId = '431831242446-c651r3f8nd3kaide989r7goilfbrkeu5.apps.googleusercontent.com';
+	//user info
+	var validationInfo = null;
+	
+	//auth response object
+	var ResponseObject = (function(){
+			return {};
+	});
+	
+	var parseParams =( function(){
+		
+		var query = window.location.hash;
+		var response = false;
+		var validationInfo = null;
+		var tokens = [];
+		if ( query !== ''){
+			response = true;
+			var response = query.substr(1).split('&');
+			for ( var i=0; i< response.length; i++){
+				tokens = response[i].split('=');
+				responseObject[tokens[0]] = tokens[1];
+				
+			}
+			console.log(responseObject);
+			window.location.hash = '';
+			validateToken(responseObject.access_token);
 		}
-	);
-			
+		return response;
+		
+	});
+	var auth =( function(){
+		window.location.href ='https://accounts.google.com/o/oauth2/auth?\
+			scope='+scope+'&\
+			state=%2Fprofile&\
+			redirect_uri=http://healqq.github.io/bettergmail/&\
+			response_type=token&\
+			client_id='+clientId;
+	});
+	var validateToken = ( function(token){
+		$.post('https://www.googleapis.com/oauth2/v1/tokeninfo',
+			{
+				access_token: token 
+			}
+		).done(function(data){
+			validationInfo = data;
+			sessionStorage.setItem('token', token);
+			console.log( data );
+		})
+	});
 	
-	
-	//we dont need to reinit this one
-	//vkManager = null;
-	ready = init();
-	return this;	
-	// wrap for vk api methods
-	//check login status
+	var checkState = ( function(){
+		var token = sessionStorage.getItem('token');
+		
+		if (token === null){
+			//not authorised
+			auth();
+		}
+		else{
+			validateToken(token);
+		}
+	});
+	return {
+		checkState: checkState,
+		parseResponse: parseParams
+	}
 	
 });
